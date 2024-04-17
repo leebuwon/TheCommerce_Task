cb2d14cd9b8dbbe49826094e97a14468c2a06cf8
leebuwon <bw1611@naver.com>
2024-04-17 19:28:23 +0900

    env: init commit

---
+++ src/main/java/com/thecommerce/thecommercetask/TheCommerceTaskApplication.java
@@ -0,0 +1,13 @@
+package com.thecommerce.thecommercetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCommerceTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheCommerceTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thecommerce/thecommercetask/TheCommerceTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecommerce.thecommercetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCommerceTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
