9a57664c3815309a4b84138b3da66e54f5509617
leebuwon <97327583+leebuwon@users.noreply.github.com>
2024-04-18 20:31:02 +0900

    Feat: UpdateUserDto valid 메시지 작성 (#18)

--- src/main/java/com/thecommerce/thecommercetask/domain/user/dto/request/UpdateUserDto.java
+++ src/main/java/com/thecommerce/thecommercetask/domain/user/dto/request/UpdateUserDto.java
@@ -17,15 +17,15 @@ import javax.validation.constraints.Size;
 public class UpdateUserDto {
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Size(min = 8, max = 30)
+    @Size(min = 8, max = 30, message = "비밀번호는 최소 8자부터 30자까지 가능합니다.")
     private String password;
 
-    @Size(min = 2, max = 30)
+    @Size(min = 2, max = 30, message = "닉네임은 최소 2글자부터 30자까지 가능합니다.")
     private String nickname;
 
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$")
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "전화번호 패턴은 000-0000-0000 입니다.")
     private String phoneNumber;
 
-    @Email
+    @Email(message = "이메일 형식을 지켜주시기 바랍니다.")
     private String email;
 }
