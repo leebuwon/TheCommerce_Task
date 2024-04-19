2bc84d2395f6575a963034b44125c3d4614cd460
leebuwon <bw1611@naver.com>
2024-04-19 16:34:22 +0900

    TestCode: DuplicateUsernameException 테스트

--- src/test/java/com/thecommerce/thecommercetask/domain/user/service/UserServiceTest.java
+++ src/test/java/com/thecommerce/thecommercetask/domain/user/service/UserServiceTest.java
@@ -2,6 +2,8 @@ package com.thecommerce.thecommercetask.domain.user.service;
 
 import com.thecommerce.thecommercetask.domain.user.dto.request.JoinUserDto;
 import com.thecommerce.thecommercetask.domain.user.entity.User;
+import com.thecommerce.thecommercetask.domain.user.exception.DuplicateUsernameException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -39,5 +42,33 @@ class UserServiceTest {
         assertEquals("이부원", user.getFullName());
         assertEquals("010-1234-1234", user.getPhoneNumber());
         assertEquals("test@naver.com", user.getEmail());
+    }
+
+    @Test
+    @DisplayName("회원Id 중복으로 인한 테스트 DuplicateUsernameException 발생")
+    void throwDuplicateUsernameException_success() {
+        JoinUserDto user1 = JoinUserDto.builder()
+                .username("testAccount")
+                .password("test1234")
+                .nickname("testNickname")
+                .fullName("이부원")
+                .phoneNumber("010-1234-1234")
+                .email("test@naver.com")
+                .build();
+
+        userService.join(user1);
+
+        JoinUserDto user2 = JoinUserDto.builder()
+                .username("testAccount")
+                .password("test1234")
+                .nickname("testNickname1234")
+                .fullName("이부원")
+                .phoneNumber("010-1234-7890")
+                .email("test1122@naver.com")
+                .build();
+
+        assertThatThrownBy(() -> userService.join(user2))
+                .isInstanceOf(DuplicateUsernameException.class)
+                .hasMessageContaining("현재 존재하는 회원이름 입니다.");
     }
 }
