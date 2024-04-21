4a823cdf4660659ac92ee959dbcc25ea162f6a9a
leebuwon <bw1611@naver.com>
2024-04-21 12:27:53 +0900

    Feat: h2 db추가하여 테스트와 로컬 환경 분리

--- src/test/java/com/thecommerce/thecommercetask/domain/user/service/UserServiceTest.java
+++ src/test/java/com/thecommerce/thecommercetask/domain/user/service/UserServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.ConstraintViolationException;
@@ -21,6 +22,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class UserServiceTest {
 
